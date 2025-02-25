12472f416c72a79904b0b845c2b2cc262bcd7e44
Elio Bonutto <ebonutto@bess-f4r1s2.clusters.42paris.fr>
2025-02-25 11:34:55 +0100

    fix: correction d erreur builtin et redirections

--- include_minishell/pipes.h
+++ include_minishell/pipes.h
@@ -6,7 +6,7 @@
 /*   By: ebonutto <ebonutto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:09:58 by ebonutto          #+#    #+#             */
-/*   Updated: 2025/02/24 12:26:08 by ebonutto         ###   ########.fr       */
+/*   Updated: 2025/02/25 11:17:40 by ebonutto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +57,9 @@ void	free_fd(int ***fd, int len);
 void	unlink_hd(t_pipes *p_data);
 
 /* Redirection */
-void	get_infile(t_pipes *p_data, t_btree *cmd);
-void	get_outfile(t_pipes *p_data, t_btree *cmd);
-void	get_redirections(t_pipes *p_data);
+int		get_infile(t_pipes *p_data, t_btree *cmd);
+int		get_outfile(t_pipes *p_data, t_btree *cmd);
+int		get_redirections(t_pipes *p_data);
 void	get_here_docs(t_config *ms_data);
 
 #endif
